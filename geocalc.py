1fcc9a2694476903485f7228a9ab7de1c37da22a
Alexander Makarevich <a_makarevich@wargaming.net>
2015-04-20 23:00:40 +0300

    Добавлен расчёт длины градуса в зависимости от параллели

---
+++ geocalc.py
@@ -0,0 +1,21 @@
+import math, cmath
+
+def calc_lat_length(latitude):
+    #print 'radians =' + str(math.radians(latitude))
+    #print 'cos =' + str(cmath.cos(math.radians(latitude)))
+    #print 'radian' + str(math.radians(5.7295))
+    latitude_degree_length_km = abs((111.3) * abs(cmath.cos(math.radians(latitude))) '''немного неточно, разобраться почему'''
+    return latitude_degree_length_km
+
+def define_moving_angle(latitude,longtitude,)
+print calc_lat_length(90)
+
+
+
+
+
+
+
+
+
+
