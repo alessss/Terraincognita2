543fbf1b490e096b88c6e502fa387297d7a72220
Alexander Makarevich <a_makarevich@wargaming.net>
2015-04-21 02:31:12 +0300

    Маршрут с правой и левой точкой в словаре

--- test_formula.py
+++ test_formula.py
@@ -41,8 +41,8 @@ def calc_azimuth_right_left(llat1,llong1,llat2,llong2):
     anglerad2 = z2 - ((2*math.pi)*math.floor((z2/(2*math.pi))) )
     angledeg = (anglerad2*180.)/math.pi
  
-    print 'Distance >> %.0f' % dist, ' [meters]'
-    print 'Initial bearing >> ', angledeg, '[degrees]'
+#    print 'Distance >> %.0f' % dist, ' [meters]'
+#    print 'Initial bearing >> ', angledeg, '[degrees]'
 
 
     azimutr = angledeg+90
@@ -51,19 +51,28 @@ def calc_azimuth_right_left(llat1,llong1,llat2,llong2):
         azimutr = azimutr - 360
     if azimutl <0:
         azimutl = azimutl + 360
-    print str(angledeg) + 'angledeg'
-    print str(azimutr) + 'azimutr'
-    print str(azimutl) + 'azimutl'
+#    print str(angledeg) + 'angledeg'
+#    print str(azimutr) + 'azimutr'
+#    print str(azimutl) + 'azimutl'
     #http://gis-lab.info/forum/viewtopic.php?f=1&t=9874
     latr = llat1 + 500 * math.cos(azimutr * math.pi / 180) / (6371000 * math.pi / 180)
     lonr = llong1 + 500 * math.sin(azimutr * math.pi / 180) / math.cos(llat1 * math.pi / 180) / (6371000 * math.pi / 180)
 
     latl = llat1 + 500 * math.cos(azimutl * math.pi / 180) / (6371000 * math.pi / 180)
     lonl = llong1 + 500 * math.sin(azimutl * math.pi / 180) / math.cos(llat1 * math.pi / 180) / (6371000 * math.pi / 180)
-
-    print 'left' + str(latl)+' '+str(lonl)
-    print 'right' + str(latr)+' '+str(lonr)
-    return(latr, lonr, latl, lonl)
+    point ={'cur_lat': llat1,
+            'cur_lon' : llong1,
+            'futlat' : llat2,
+            'futlon' : llong2,
+            'azimuth' : angledeg,
+            'lonr' : lonr,
+            'latr' : latr,
+            'lonl' : lonl,
+            'latr' : latr,
+            'distance' : dist}
+#    print 'left' + str(latl)+' '+str(lonl)
+#    print 'right' + str(latr)+' '+str(lonr)
+    return(point)
 
 #print calc_azimuth_right_left(53.917534443,27.7704913427,53.910025557,27.7620886573)
 full_route = []
@@ -72,6 +81,7 @@ i = 0
 for point in route:
     a = route[i]
     b = route[i+1]
-    print 'point' + ' ' +str(i+1)
-    print calc_azimuth_right_left(a.get('lat'),a.get('lon'),b.get('lat'),b.get('lon'))
+    full_route_dict = calc_azimuth_right_left(a.get('lat'),a.get('lon'),b.get('lat'),b.get('lon'))
+    full_rote = full_route.append(full_route_dict)
+    print full_route
     i = i+1
