cf2aab03e267dc20732b4956ca1731f554083fb8
Alexander Makarevich <a_makarevich@wargaming.net>
2015-04-21 01:32:38 +0300

    Тестовая формула вынесена в функцию

--- geocalc.py
+++ geocalc.py
@@ -10,17 +10,18 @@ def calc_lat_length(latitude):
 # dont know why inaccurate
     return latitude_degree_length_km
 
-def calc_lat_lon_change(latitude, latitude_previous, longtitude, longtitude_previous):
+def calc_lat_lon_change_distance_angle(latitude, latitude_previous, longtitude, longtitude_previous):
     latitude_move = (latitude_previous - latitude) * calc_lat_length(latitude)
     longtitude_move = (longtitude_previous - longtitude) * longtitude_degree_length_km
-    return (latitude_move, longtitude_move)
+    distance = math.sqrt(latitude_move*latitude_move+longtitude_move*longtitude_move)
+    beta = math.acos(latitude_move/distance)
+    return (latitude_move, longtitude_move, distance,beta)
 
-#def define_moving_angle(latitude,longtitude)
-#print calc_lat_length(90)
 
 
-print calc_lat_lon_change(53.91251,53.91278,27.77017,27.76944)
 
+print calc_lat_lon_change_distance_angle(53.91378,53.91522,27.76629,27.76258)
+print calc_lat_lon_change_distance_angle(53.91378,53.91522,27.76629,27.76258)
 
 
 
--- test_formula.py
+++ test_formula.py
@@ -1,37 +1,58 @@
 #http://gis-lab.info/qa/great-circles.html
 import math
- 
-rad = 6372795
-llat1 = 53.91378
-llong1 = 27.76629
-llat2 = 53.91522
-llong2 = 27.76258
-lat1 = llat1*math.pi/180.
-lat2 = llat2*math.pi/180.
-long1 = llong1*math.pi/180.
-long2 = llong2*math.pi/180.
-cl1 = math.cos(lat1)
-cl2 = math.cos(lat2)
-sl1 = math.sin(lat1)
-sl2 = math.sin(lat2)
-delta = long2 - long1
-cdelta = math.cos(delta)
-sdelta = math.sin(delta)
-y = math.sqrt(math.pow(cl2*sdelta,2)+math.pow(cl1*sl2-sl1*cl2*cdelta,2))
-x = sl1*sl2+cl1*cl2*cdelta
-ad = math.atan2(y,x)
-dist = ad*rad
-x = (cl1*sl2) - (sl1*cl2*cdelta)
-y = sdelta*cl2
-z = math.degrees(math.atan(-y/x))
 
-if (x < 0):
-    z = z+180.
+def calc_azimuth_right_left(llat1,llong1,llat2,llong2):
+    rad = 6372795
+    llat1 = 53.91378
+    llong1 = 27.76629
+    llat2 = 53.9162543794
+    llong2 = 27.7599155092
+    lat1 = llat1*math.pi/180.
+    lat2 = llat2*math.pi/180.
+    long1 = llong1*math.pi/180.
+    long2 = llong2*math.pi/180.
+    cl1 = math.cos(lat1)
+    cl2 = math.cos(lat2)
+    sl1 = math.sin(lat1)
+    sl2 = math.sin(lat2)
+    delta = long2 - long1
+    cdelta = math.cos(delta)
+    sdelta = math.sin(delta)
+    y = math.sqrt(math.pow(cl2*sdelta,2)+math.pow(cl1*sl2-sl1*cl2*cdelta,2))
+    x = sl1*sl2+cl1*cl2*cdelta
+    ad = math.atan2(y,x)
+    dist = ad*rad
+    x = (cl1*sl2) - (sl1*cl2*cdelta)
+    y = sdelta*cl2
+    z = math.degrees(math.atan(-y/x))
+
+    if (x < 0):
+        z = z+180.
  
-z2 = (z+180.) % 360. - 180.
-z2 = - math.radians(z2)
-anglerad2 = z2 - ((2*math.pi)*math.floor((z2/(2*math.pi))) )
-angledeg = (anglerad2*180.)/math.pi
+    z2 = (z+180.) % 360. - 180.
+    z2 = - math.radians(z2)
+    anglerad2 = z2 - ((2*math.pi)*math.floor((z2/(2*math.pi))) )
+    angledeg = (anglerad2*180.)/math.pi
  
-print 'Distance >> %.0f' % dist, ' [meters]'
-print 'Initial bearing >> ', angledeg, '[degrees]'
+    print 'Distance >> %.0f' % dist, ' [meters]'
+    print 'Initial bearing >> ', angledeg, '[degrees]'
+
+
+    azimutr = angledeg+90
+    azimutl = angledeg-90
+    if azimutr > 360:
+        azimutr = azimutr - 360
+    if azimutl <0:
+        azimutl = azimutl + 360
+    print str(angledeg) + 'angledeg'
+    print str(azimutr) + 'azimutr'
+    print str(azimutl) + 'azimutl'
+    #http://gis-lab.info/forum/viewtopic.php?f=1&t=9874
+    latr = llat1 + 500 * math.cos(azimutr * math.pi / 180) / (6371000 * math.pi / 180)
+    lonr = llong1 + 500 * math.sin(azimutr * math.pi / 180) / math.cos(llat1 * math.pi / 180) / (6371000 * math.pi / 180)
+
+    latl = llat1 + 500 * math.cos(azimutl * math.pi / 180) / (6371000 * math.pi / 180)
+    lonl = llong1 + 500 * math.sin(azimutl * math.pi / 180) / math.cos(llat1 * math.pi / 180) / (6371000 * math.pi / 180)
+
+    return(latr, lonr, latl, lonl)
+    print latr, lonr, latl, lonl
