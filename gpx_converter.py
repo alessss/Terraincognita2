8b55404a8c8809b56682d4191b448bd2c6d39715
Alexander Makarevich <a_makarevich@wargaming.net>
2015-04-26 01:21:42 +0300

    Конвертация и расчёт посещённой площади напрямую из gpx файла в функции

--- gpx_converter.py
+++ gpx_converter.py
@@ -1,19 +1,28 @@
 import time
 from tiles_calculations import calc_square
 import tiles_calculations
-__author__ = 'a_makarevich'
-file = open('D:\Terra incognita\long_gpx.gpx', 'r')
-track_list = []
-track_dict = {}
-for line in file:
-    if '<trkpt' in line:
-        lat_start = line.find('"')
-        lat_end = line.find('"',lat_start+1)
-        lat = line[lat_start+1:lat_end]
-        lon_start = line.find('"', lat_end+1)
-        lon_end = line.find('"',lon_start+1)
-        lon = line[lon_start+1:lon_end]
-        track_dict = {'lat': lat, 'lon': lon}
-        track_list.append(track_dict)
-#        time.sleep(1)
-print tiles_calculations.calc_square(track_list)
+import argparse
+
+#parser = argparse.ArgumentParser(prog='PROG')
+#parser.add_argument('--file', type = str)
+#print parser
+def file_worker(filename):
+    file = open(filename, 'r')
+    #file = open(parser.parse_args('--file'), 'r')
+    track_list = []
+    track_dict = {}
+    for line in file:
+        if '<trkpt' in line:
+            lat_start = line.find('"')
+            lat_end = line.find('"',lat_start+1)
+            lat = line[lat_start+1:lat_end]
+            lon_start = line.find('"', lat_end+1)
+            lon_end = line.find('"',lon_start+1)
+            lon = line[lon_start+1:lon_end]
+            track_dict = {'lat': lat, 'lon': lon}
+            track_list.append(track_dict)
+#           time.sleep(1)
+    return track_list
+
+
+print tiles_calculations.calc_square(file_worker('D:\\Terra incognita\\0019.gpx'))
